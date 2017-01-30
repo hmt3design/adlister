7a62bad9ff7dfcfb745a8a985c2ee2d38bf8fc11
hmt3design <hmt3design@gmail.com>
2017-01-30 12:29:59 -0600

    013017 - add name pair

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -5,11 +5,11 @@ import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = {"/hello"})
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        Object counter = req.getParameter("counter");
+        Object counter = request.getParameter("counter");
         int count;
 
         if (counter != null) {
@@ -18,12 +18,18 @@ public class HelloWorldServlet extends HttpServlet {
         } else {
             count = 1;
         }
+        request.setAttribute("counter", count);
 
-        req.setAttribute("counter", count);
+        response.setContentType("text/html");
+        PrintWriter writer= response.getWriter();
+        String name = request.getParameter("name");
+        if (name != null) {
+            writer.println("<h1>Hello, " + name + "!");
+        } else {
+            writer.println("<h1>Hello, World!</h1>");
+        }
 
-        res.setContentType("text/html");
-        PrintWriter out = res.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+
 
     }
 }
